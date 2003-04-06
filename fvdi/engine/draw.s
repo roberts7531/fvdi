8a69ee979e1b6e5439e135e7e675dd2aa850ab1c
Johan Klockars <johan@klockars.net>
2003-04-06 13:42:12 +0000

    Support for rounded boxes added. Bezier code converted from assembly to C.

--- fvdi/engine/draw.s
+++ fvdi/engine/draw.s
@@ -1,7 +1,7 @@
 *****
 * fVDI drawing functions
 *
-* $Id: draw.s,v 1.4 2002-07-10 22:12:25 johan Exp $
+* $Id: draw.s,v 1.5 2003-04-06 13:42:12 johan Exp $
 *
 * Copyright 1997-2002, Johan Klockars 
 * This software is licensed under the GNU General Public License.
@@ -31,15 +31,19 @@ transparent	equ	1		; Fall through?
 	xref	_pattern_ptrs
 	xref	_filled_poly,_filled_poly_m,_clc_arc,_wide_line,_calc_bez
 	xref	_arc_split,_arc_min,_arc_max
+	xref	_lib_v_bez,_rounded_box
 
 	xdef	v_pline,v_circle,v_arc,v_ellipse,v_ellarc,v_pie,v_ellpie
 	xdef	v_pmarker
 	xdef	v_fillarea
 	xdef	lib_v_pline
+	xdef	_lib_v_pline
+	xdef	v_rbox,v_rfbox
 
 	xdef	_default_line
 	xdef	_fill_poly,_hline,_fill_spans
 	xdef	_c_pline
+	xdef	_v_bez_accel
 
 
 	text
@@ -107,176 +111,57 @@ v_bez:
 	add.w	#22,a7
 	used_d1
 	done_return
-	
+
+* v_bez_accel((long)vwk + 1, points, 
+*             (num_points << 16) | 1, *par->totmoves, xmov,
+*             pattern, vwk->line.colour, vwk->mode);
+_v_bez_accel:
+	movem.l	d2-d6,-(a7)
+	move.l	5*4+4(a7),d0
+	and.w	#$fffe,d0
+	move.l	d0,a0
+	move.l	5*4+8(a7),d1
+	move.l	5*4+12(a7),d2
+	move.l	5*4+16(a7),d3
+	move.l	5*4+20(a7),d4
+	move.l	5*4+24(a7),d5
+	moveq	#0,d6
+	move.w	vwk_mode(a0),d6
+	move.l	d0,a0
+	move.l	vwk_real_address(a0),a1
+	move.l	wk_r_line(a1),d0
+	move.l	d0,a1
+	move.l	5*4+28(a7),d0
+	addq.l	#1,a0
+	jsr	(a1)
+	movem.l	(a7)+,d2-d6
+	rts	
 
 * lib_v_bez - Standard Library function
 * Todo: ?
 * In:	a1	Parameters  lib_v_bez(num_pts, points, bezarr, extent, totpoints, totmoves)
 *	a0	VDI struct
 lib_v_bez:
-	sub.w	#10,a7
-	move.l	a7,a2
-	movem.l	a0-a1,-(a7)
-	move.l	a2,-(a7)
-	move.l	18(a1),-(a7)
-	pea	2(a2)
-	move.l	a0,d0
-	add.w	#vwk_clip_rectangle,d0
-	move.l	d0,2(a2)
-	pea	6(a2)
-	moveq	#0,d0
-	move.w	0(a1),d0
-	move.l	d0,-(a7)	; marks = num_pts   ?
-	move.l	d0,-(a7)
-	move.w	vwk_bezier_depth_scale(a0),d0
-;	move.w	#0,d0
-	move.l	d0,-(a7)
-	move.l	2(a1),-(a7)
-	move.l	6(a1),-(a7)
-	bra	.bez_loop_end
-.bez_loop:
-	jsr	_calc_bez	; (ch *marks, sh *points, sh flags, sh maxpnt, sh maxin, sh **xmov, sh **xpts, sh *pnt_mv_cnt, sh *x_used)
-	tst.l	d0
-	bge	.done
-	tst.w	9*4+2*4(a7)		; xused?
-	beq	.normal_line
-	addq.w	#1,8+2(a7)
-.bez_loop_end:
-	move.l	9*4(a7),a0
-	move.l	a0,d0			; Restore clip rectangle pointer
-	add.w	#vwk_clip_rectangle,d0
-	move.l	24(a7),a2
-	move.l	d0,(a2)
-	move.l	vwk_real_address(a0),a2
-	move.w	8+2(a7),d0
-	cmp.w	wk_drawing_bezier_depth_scale_min(a2),d0
-;	cmp.w	#9,8+2(a7)
-	ble	.bez_loop
-
-	add.w	#9*4,a7		; Should we ever get here?
-	movem.l	(a7),a0-a1
-	moveq	#0,d0
-	move.w	d0,0(a7)	; No allocated memory etc
-	move.l	d0,4(a7)
-	move.l	d0,8(a7)
-	move.w	0(a1),d0
-	lea	2(a1),a0
-	bra	.finish_up
-
-.done:
-	add.w	#9*4,a7
-;	move.l	0(a7),a0
-	movem.l	0(a7),a0-a1
-	subq.l	#6,a7
-	move.w	d0,0(a7)
-
-	movem.l	d2-d6,-(a7)
-	move.w	d0,d6
-	move.l	5*4+6+2*4+2(a7),a2		; Points
-;	move.w	5*4+6+2*4+0(a7),d3		; Move point count
-	move.l	18(a1),a1
-	move.w	(a1),d3
-	move.l	5*4+6+2*4+6(a7),d4		; Move indices
-
-	move.l	vwk_line_colour(a0),d0
-	cmp.w	#1,vwk_line_width(a0)
-	bhi	.wide_bez			; Do wide lines too!!!
-
-.no_wide_bez:
-	move.w	vwk_line_user_mask(a0),d5
-	move.w	vwk_line_type(a0),d1
-	cmp.w	#7,d1
-	beq	.bez_userdef
-	lea	_line_types,a1
-	subq.w	#1,d1
-	add.w	d1,d1
-	move.w	0(a1,d1.w),d5
-.bez_userdef:
-
-	move.l	vwk_real_address(a0),a1
-	move.l	wk_r_line(a1),d1
-	move.l	d1,a1
-
-	move.l	a2,d1
-	move.w	d6,d2
-	swap	d2
-	move.w	#1,d2			; Should be 1 for move handling
-	moveq	#0,d6
-	move.w	vwk_mode(a0),d6
-	addq.l	#1,a0
-	jsr	(a1)
-
-.end_bez_draw:		; .end
-	movem.l	(a7)+,d2-d6
-
-	move.w	0(a7),d0
-	addq.l	#6,a7
-	move.l	2*4+2(a7),a0
-
-.finish_up:
-	move.l	d0,a2
-	bsr	bezier_size
-	movem.l	(a7)+,a0-a1
-	move.l	10(a1),a0
-	move.l	d0,(a0)+
-	move.l	d1,(a0)
-	move.l	14(a1),a0
-	move.w	a2,(a0)
-	move.l	18(a1),a0
-	move.w	0(a7),(a0)
-	move.l	2(a7),d0
-	beq	.no_free
-	move.l	d0,-(a7)
-	bsr	_free_block
-	addq.l	#4,a7
-.no_free:
-	add.w	#10,a7
-	rts
-		
-.normal_line:
-	add.w	#9*4,a7
-	movem.l	(a7),a0-a1
-	bsr	lib_v_pline
-	movem.l	(a7),a0-a1
-	moveq	#0,d0
-	move.w	d0,2*4+0(a7)	; No allocated memory etc
-	move.l	d0,2*4+2(a7)
-	move.l	d0,2*4+6(a7)
-	move.w	0(a1),d0
-	move.l	2(a1),a0
-	bra	.finish_up
-
-.wide_bez:
-	move.l	d0,d1
-	clr.l	-(a7)
-	bsr	_allocate_block
-	addq.l	#4,a7
-	tst.l	d0
-	beq	.no_wide_bez
-
-	moveq	#0,d2
-	move.w	vwk_mode(a0),d2
-	move.l	d2,-(a7)
-
-	move.l	d0,-(a7)	; For _free_block below (and _wide_line call)
-	move.l	d1,-(a7)
-	ext.l	d6
-	move.l	d6,-(a7)
-	move.l	a2,-(a7)
+  ifeq 0
+	movem.l	d0-d2/a2,-(a7)
+	move.l	a1,-(a7)
 	move.l	a0,-(a7)
-	jsr	_wide_line
-	add.w	#16,a7
-
-	bsr	_free_block
-	addq.l	#8,a7
-
-	bra	.end_bez_draw
+	jsr	_lib_v_bez
+	move.l	(a7)+,a0
+	move.l	(a7)+,a1
+	movem.l	(a7)+,d0-d2/a2
+	rts
+  endc
+; Removed old code
 
 
 * lib_v_pline - Standard Library function
 * Todo: ?
 * In:	a1	Parameters  lib_v_pline(num_pts, points)
 *	a0	VDI struct
+_lib_v_pline:
+	move.l	4(a7),a0
+	move.l	8(a7),a1
 lib_v_pline:
 ;	use_special_stack
 ;	move.w	#0,d0			; Background colour
@@ -803,13 +688,13 @@ v_ellarc:
 	move.l	a0,-(a7)
 	jsr	_clc_arc	; vwk, gdb_code, xc, yc, xrad, yrad, beg_ang, end_ang, del_ang, n_steps, colour, pattern, points, mode, interior_style
 
-	add.w	#40,a7
+	add.w	#10*4,a7
 
 .ellarc_finish:
-	addq.l	#8,a7
+	addq.l	#2*4,a7
 
 	bsr	_free_block
-	add.w	#12,a7
+	add.w	#3*4,a7
 
 .ellarc_end:
 	movem.l	(a7)+,d2-d7
@@ -887,13 +772,13 @@ v_ellpie:
 	move.l	a0,-(a7)
 	jsr	_clc_arc	; vwk, gdb_code, xc, yc, xrad, yrad, beg_ang, end_ang, del_ang, n_steps, colour, pattern, points, mode, interior_style
 
-	add.w	#40,a7
+	add.w	#10*4,a7
 
 .ellpie_finish:
-	addq.l	#8,a7
+	addq.l	#2*4,a7
 
 	bsr	_free_block
-	add.w	#12,a7
+	add.w	#3*4,a7
 
 .ellpie_end:
 	movem.l	(a7)+,d2-d7
@@ -961,429 +846,169 @@ v_ellipse:
 	move.l	a0,-(a7)
 	jsr	_clc_arc	; vwk, gdb_code, xc, yc, xrad, yrad, 0, 0, 3600, n_steps, colour, pattern, points, mode, interior_style
 
-	add.w	#48,a7
+	add.w	#12*4,a7
 
 	bsr	_free_block
-	add.w	#12,a7
+	add.w	#3*4,a7
 
 .ellipse_end:
 	movem.l	(a7)+,d2-d5
 	used_d1
 	done_return
   endc
+; Removed old code
 
-  ifne 0
-;	move.w	#0,d0			; Background colour
-;	swap	d0
-	move.l	vwk_line_colour(a0),d0
 
+	dc.b	0,"v_rbox",0
+* v_rbox - Standard Trap function
+* Todo: ?
+* In:   a1      Parameter block
+*       a0      VDI struct
+v_rbox:
 	uses_d1
+	subq.l	#4,a7
+	move.l	ptsin(a1),0(a7)		; Edges
 
-	movem.l	d2-d7/a3-a6,-(a7)
-	move.l	ptsin(a1),a2
+	move.l	a7,a1
+	bsr	lib_v_rbox
+	addq.l	#4,a7
+	used_d1
+	done_return			; Should be real_return
 
-;	move.w	vwk_line_user_mask(a0),d5
-;	move.w	vwk_line_type(a0),d1
-;	cmp.w	#7,d1
-;	beq	.userdef
-;	lea	_line_types,a1
-;	subq.w	#1,d1
-;	add.w	d1,d1
-;	move.w	0(a1,d1.w),d5
-;.userdef:
+* lib_v_rbox - Standard library function
+* Todo: ?
+* In:	a1	Parameters  lib_v_rbox(points)
+*	a0	VDI struct
+lib_v_rbox:
+	movem.l	d2-d5,-(a7)
 
-	move.l	vwk_real_address(a0),a1
-	move.l	wk_r_get_colour(a1),a1	; Index to real colour
-	jsr	(a1)
+	moveq	#0,d1
+	moveq	#0,d2
+	moveq	#0,d3
+	moveq	#0,d4
+	move.l	0(a1),a1
+	movem.w	0(a1),d1-d4
 
-	movem.w	4(a2),d2-d3
-	tst.w	d2
-	beq	.ellipse_end
+	cmp.w	d1,d3
+	bge	.rb_xorder
+	exg	d1,d3
+.rb_xorder:
+	cmp.w	d2,d4
+	bge	.rb_yorder
+	exg	d2,d4
+.rb_yorder:
 
-	move.w	d2,d1
-	mulu	d2,d1
-	move.l	d1,d5		; d5 = a * a	(a2)
-	move.w	d3,d1
-	mulu	d3,d1
-	move.l	d1,a6		; a6 = b * b	(b2)
-;	sub.l	a3,a3		; a3 = 0	(b2x)
-	move.l	d5,d6
-	mulu	d3,d6
-	neg.l	d6		; Was nothing (a5 now 0 - a2 * b   (b2x - a2 * b))
-	move.l	d6,a5		; a5 = a2 * b	(a2y)
-	move.l	d5,d6
-	add.l	a5,d6		; Was	sub.l	a5,d6		; d6 = a2 - a2y	(dec1)
-	move.l	a6,d7
-	add.l	d7,d7		; d7 = b2 * 2	(dec2)
-	move.l	d7,d3
-	add.l	d6,d3
-	add.l	a5,d3		; Was	sub.l	a5,d3		; d3 = dec2 + dec1 - a2y	(dec)
-	add.l	d6,d6
-	add.l	d6,d6		; d6 = dec1 * 4	(dec1)
-	move.l	d7,d1
-	add.l	d7,d7
-	add.l	d1,d7		; d7 = dec2 * 3	(dec2)
-
-	clr.w	d4
-	swap	d4
-	move.w	6(a2),d4	; d4 = x, y
-
-	cmp.w	#1,vwk_mode(a0)
-	bne	.nonsolid
-	move.l	d0,a1		; Remember colours
-	move.l	#0,-(a7)	; Get a memory block of any size (hopefully large)
+	clr.l	-(a7)
 	bsr	_allocate_block
 	addq.l	#4,a7
 	tst.l	d0
-	bne	.table_ellipse
-	move.l	a1,d0		; Restore colours
-	
-.nonsolid:			; Couldn't use the table routine
-	clr.l	-(a7)
-	move.l	a0,-(a7)
+	beq	.rbox_end
 
-	move.w	vwk_mode(a0),-(a7)
-	bsr	setup_plot
-	addq.l	#2,a7
+	move.w	vwk_fill_interior(a0),d5
+	swap	d5
+	move.w	vwk_fill_style(a0),d5
+	move.l	d5,-(a7)
+	moveq	#0,d5
+	move.w	vwk_mode(a0),d5
+	move.l	d5,-(a7)
 
-;	move.l	a7,a0			; a0 no longer -> VDI struct!
+	move.l	d0,-(a7)	; For _free_block below (and _clc_arc call)
 
-	bra	.xstep_end
-
-.xstep_loop:
-	movem.w	(a2),d1-d2
-
-	add.w	d4,d2
-	swap	d4
-	add.w	d4,d1
-	move.l	(a7),a0
-	bsr	clip_point
-	lbgt	.skip1,1
-	move.l	a7,a0
-	move	#1,ccr
-	jsr	(a1)		; xc + x, yc + y
- label .skip1,1
-	sub.w	d4,d1
-	sub.w	d4,d1
-	move.l	(a7),a0
-	bsr	clip_point
-	lbgt	.skip2,2
-	move.l	a7,a0
-	move	#1,ccr
-	jsr	(a1)		; xc - x, yc + y
- label .skip2,2
-	swap	d4
-	sub.w	d4,d2
-	sub.w	d4,d2
-	move.l	(a7),a0
-	bsr	clip_point
-	lbgt	.skip3,3
-	move.l	a7,a0
-	move	#1,ccr
-	jsr	(a1)		; xc - x, yc - y
- label .skip3,3
-	swap	d4
-	add.w	d4,d1
-	add.w	d4,d1
-	move.l	(a7),a0
-	bsr	clip_point
-	lbgt	.skip4,4
-	move.l	a7,a0
-	move	#1,ccr
-	jsr	(a1)		; xc + x, yc - y
- label .skip4,4
-	swap	d4
-
-	tst.l	d3		; dec >= 0?
-	bmi	.no_ystep
-	add.l	d6,d3		; dec += dec1
-	add.l	d5,d6
-	add.l	d5,d6
-	add.l	d5,d6
-	add.l	d5,d6		; dec1 += 4 * a2
-	subq.w	#1,d4		; y--
-	add.l	d5,a5		; Was	sub.l	d5,a5		; a2y -= a2
-.no_ystep:
-	add.l	d7,d3		; dec += dec2
-	add.l	a6,d7
-	add.l	a6,d7
-	add.l	a6,d7
-	add.l	a6,d7		; dec2 += 4 * b2
-	add.l	a6,a5		; Was	add.l	a6,a3
-
-	add.l	#$10000,d4
-.xstep_end
-	cmp.l	#0,a5		; Was	cmp.l	a5,a3
-	ble	.xstep_loop
-
-	movem.w	4(a2),d2-d3
-;	move.w	d2,d1
-;	mulu	d2,d1
-;	move.l	d1,d5		; d5 = a * a	(a2)
-;	move.w	d3,d1
-;	mulu	d3,d1
-;	move.l	d1,a6		; a6 = b * b	(b2)
-;	sub.l	a5,a5		; a5 = 0	(a2y)
-	move.l	a6,d6
-	mulu	d2,d6
-	move.l	d6,a5		; Was	move.l	d6,a3		; a3 = b2 * a	(b2x)
-	move.l	a6,d6
-	sub.l	a5,d6		; Was	sub.l	a3,d6		; d6 = b2 - b2x	(dec1)
-	move.l	d5,d7
-	add.l	d7,d7		; d7 = a2 * 2	(dec2)
-	move.l	d7,d3
-	add.l	d6,d3
-	sub.l	a5,d3		; Was	sub.l	a3,d3		; d3 = dec2 + dec1 - b2x	(dec)
-	add.l	d6,d6
-	add.l	d6,d6		; d6 = dec1 * 4	(dec1)
-	move.l	d7,d1
-	add.l	d7,d7
-	add.l	d1,d7		; d7 = dec2 * 3	(dec2)
-
-	move.w	4(a2),d4
-	swap	d4		; d4 = x, y
-	clr.w	d4
-
-	bra	.ystep_end
-
-.ystep_loop:
-	movem.w	(a2),d1-d2
-
-	add.w	d4,d2
-	swap	d4
-	add.w	d4,d1
-	move.l	(a7),a0
-	bsr	clip_point
-	lbgt	.skip1,1
-	move.l	a7,a0
-	move	#1,ccr
-	jsr	(a1)		; xc + x, yc + y
- label .skip1,1
-	sub.w	d4,d1
-	sub.w	d4,d1
-	move.l	(a7),a0
-	bsr	clip_point
-	lbgt	.skip2,2
-	move.l	a7,a0
-	move	#1,ccr
-	jsr	(a1)		; xc - x, yc + y
- label .skip2,2
-	swap	d4
-	sub.w	d4,d2
-	sub.w	d4,d2
-	move.l	(a7),a0
-	bsr	clip_point
-	lbgt	.skip3,3
-	move.l	a7,a0
-	move	#1,ccr
-	jsr	(a1)		; xc - x, yc - y
- label .skip3,3
-	swap	d4
-	add.w	d4,d1
-	add.w	d4,d1
-	move.l	(a7),a0
-	bsr	clip_point
-	lbgt	.skip4,4
-	move.l	a7,a0
-	move	#1,ccr
-	jsr	(a1)		; xc + x, yc - y
- label .skip4,4
-	swap	d4
-
-	tst.l	d3		; dec >= 0?
-	bmi	.no_xstep
-	add.l	d6,d3		; dec += dec1
-	add.l	a6,d6
-	add.l	a6,d6
-	add.l	a6,d6
-	add.l	a6,d6		; dec1 += 4 * b2
-	sub.l	#$10000,d4	; x--
-	sub.l	a6,a5		; b2x -= b2
-.no_xstep:
-	add.l	d7,d3		; dec += dec2
-	add.l	d5,d7
-	add.l	d5,d7
-	add.l	d5,d7
-	add.l	d5,d7		; dec2 += 4 * b2
-	sub.l	d5,a5		; Was	add.l	d5,a5
-
-	addq.w	#1,d4
-.ystep_end
-	cmp.l	#0,a5		; Was	cmp.l	a3,a5
-	bgt	.ystep_loop	; Was	bls	.ystep_loop
-
-	add.w	#8,a7
-.ellipse_end:
-	movem.l	(a7)+,d2-d7/a3-a6
-	used_d1
-	done_return
-
-* When a table can be used
-*
-.table_ellipse:
-	exg	a1,d0
+	bsr	col_pat
+	move.l	d5,-(a7)
 	move.l	d0,-(a7)
-	move.l	a1,-(a7)
-
-	move.l	d5,d0
-	add.l	d0,d0
-	add.l	d0,d0			; 4 * a2
-	move.l	a6,a3
-	add.l	a3,a3
-	add.l	a3,a3			; 4 * b2
-	bra	.txstep_end
-
-.txstep_loop:
-	movem.w	(a2),d1-d2
-
-	add.w	d4,d2
-	swap	d4
-	add.w	d4,d1
-	bsr	clip_point
-	lbgt	.skip1,1
-	move.w	d1,(a1)+		; xc + x, yc + y
-	move.w	d2,(a1)+
- label .skip1,1
-	sub.w	d4,d1
-	sub.w	d4,d1
-	bsr	clip_point
-	lbgt	.skip2,2
-	move.w	d1,(a1)+		; xc - x, yc + y
-	move.w	d2,(a1)+
- label .skip2,2
-	swap	d4
-	sub.w	d4,d2
-	sub.w	d4,d2
-	bsr	clip_point
-	lbgt	.skip3,3
-	move.w	d1,(a1)+		; xc - x, yc - y
-	move.w	d2,(a1)+
- label .skip3,3
-	swap	d4
-	add.w	d4,d1
-	add.w	d4,d1
-	bsr	clip_point
-	lbgt	.skip4,4
-	move.w	d1,(a1)+		; xc + x, yc - y
-	move.w	d2,(a1)+
- label .skip4,4
-	swap	d4
-
-	tst.l	d3		; dec >= 0?
-	bmi	.tno_ystep
-	add.l	d6,d3		; dec += dec1
-	add.l	d0,d6		; dec1 += 4 * a2
-	subq.w	#1,d4		; y--
-	add.l	d5,a5		; Was	sub.l	d5,a5		; a2y -= a2
-.tno_ystep:
-	add.l	d7,d3		; dec += dec2
-	add.l	a3,d7		; dec2 += 4 * b2
-	add.l	a6,a5		; Was	add.l	a6,a3
-
-	add.l	#$10000,d4
-.txstep_end
-	cmp.l	#0,a5		; Was	cmp.l	a5,a3
-	ble	.txstep_loop
-
-	movem.w	4(a2),d2-d3
-	move.l	a6,d6
-	mulu	d2,d6
-	move.l	d6,a5		; Was	move.l	d6,a3		; a3 = b2 * a	(b2x)
-	move.l	a6,d6
-	sub.l	a5,d6		; Was	sub.l	a3,d6		; d6 = b2 - b2x	(dec1)
-	move.l	d5,d7
-	add.l	d7,d7		; d7 = a2 * 2	(dec2)
-	move.l	d7,d3
-	add.l	d6,d3
-	sub.l	a5,d3		; Was	sub.l	a3,d3		; d3 = dec2 + dec1 - b2x	(dec)
-	add.l	d6,d6
-	add.l	d6,d6		; d6 = dec1 * 4	(dec1)
-	move.l	d7,d1
-	add.l	d7,d7
-	add.l	d1,d7		; d7 = dec2 * 3	(dec2)
-
-	move.w	4(a2),d4
-	swap	d4		; d4 = x, y
-	clr.w	d4
-
-	bra	.tystep_end
-
-.tystep_loop:
-	movem.w	(a2),d1-d2
-
-	add.w	d4,d2
-	swap	d4
-	add.w	d4,d1
-	bsr	clip_point
-	lbgt	.skip1,1
-	move.w	d1,(a1)+	; xc + x, yc + y
-	move.w	d2,(a1)+
- label .skip1,1
-	sub.w	d4,d1
-	sub.w	d4,d1
-	bsr	clip_point
-	lbgt	.skip2,2
-	move.w	d1,(a1)+	; xc - x, yc + y
-	move.w	d2,(a1)+
- label .skip2,2
-	swap	d4
-	sub.w	d4,d2
-	sub.w	d4,d2
-	bsr	clip_point
-	lbgt	.skip3,3
-	move.w	d1,(a1)+	; xc - x, yc - y
-	move.w	d2,(a1)+
- label .skip3,3
-	swap	d4
-	add.w	d4,d1
-	add.w	d4,d1
-	bsr	clip_point
-	lbgt	.skip4,4
-	move.w	d1,(a1)+	; xc + x, yc - y
-	move.w	d2,(a1)+
- label .skip4,4
-	swap	d4
-
-	tst.l	d3		; dec >= 0?
-	bmi	.tno_xstep
-	add.l	d6,d3		; dec += dec1
-	add.l	a3,d6		; dec1 += 4 * b2
-	sub.l	#$10000,d4	; x--
-	sub.l	a6,a5		; b2x -= b2
-.tno_xstep:
-	add.l	d7,d3		; dec += dec2
-	add.l	d0,d7		; dec2 += 4 * b2
-	sub.l	d5,a5		; Was	add.l	d5,a5
-
-	addq.w	#1,d4
-.tystep_end
-	cmp.l	#0,a5		; Was	cmp.l	a3,a5
-	bgt	.tystep_loop	; Was	bls	.ystep_loop
-
-	move.l	(a7)+,d1	; Table address
-	move.l	(a7)+,d0	; Colours
-	move.l	a1,d2
-	sub.l	d1,d2
-	lsr.l	#2,d2		; Number of points
-	swap	d2
-	clr.w	d2		; Coordinate mode
-
-	move.l	d1,-(a7)	; For free_block below
-	
-	move.l	vwk_real_address(a0),a1
-	move.l	wk_r_set_pixel(a1),a1
-	clr.l	-(a7)
+	move.l	d4,-(a7)
+	move.l	d3,-(a7)
+	move.l	d2,-(a7)
+	move.l	d1,-(a7)
+	move.l	#8,-(a7)	; rbox
 	move.l	a0,-(a7)
-	move.l	a7,a0
-	addq.l	#1,a0		; Table operation
-	jsr	(a1)
-	addq.l	#8,a7
+	jsr	_rounded_box	; vwk, gdb_code, x1, y1, x2, y2, colour, pattern, points, mode, interior_style
+
+	add.w	#8*4,a7
 
 	bsr	_free_block
-	addq.l	#4,a7
+	add.w	#3*4,a7
 
-	bra	.ellipse_end
-  endc
+.rbox_end:
+	movem.l	(a7)+,d2-d5
+	rts
+
+
+	dc.b	0,0,"v_rfbox",0
+* v_rfbox - Standard Trap function
+* Todo: ?
+* In:   a1      Parameter block
+*       a0      VDI struct
+v_rfbox:
+	uses_d1
+	subq.l	#4,a7
+	move.l	ptsin(a1),0(a7)		; Edges
+
+	move.l	a7,a1
+	bsr	lib_v_rfbox
+	addq.l	#4,a7
+	used_d1
+	done_return			; Should be real_return
+
+* lib_v_rfbox - Standard library function
+* Todo: ?
+* In:	a1	Parameters  lib_v_rfbox(points)
+*	a0	VDI struct
+lib_v_rfbox:
+	movem.l	d2-d5,-(a7)
+
+	moveq	#0,d1
+	moveq	#0,d2
+	moveq	#0,d3
+	moveq	#0,d4
+	move.l	0(a1),a1
+	movem.w	0(a1),d1-d4
+
+	cmp.w	d1,d3
+	bge	.rfb_xorder
+	exg	d1,d3
+.rfb_xorder:
+	cmp.w	d2,d4
+	bge	.rfb_yorder
+	exg	d2,d4
+.rfb_yorder:
+
+	clr.l	-(a7)
+	bsr	_allocate_block
+	addq.l	#4,a7
+	tst.l	d0
+	beq	.rfbox_end
+
+	move.w	vwk_fill_interior(a0),d5
+	swap	d5
+	move.w	vwk_fill_style(a0),d5
+	move.l	d5,-(a7)
+	moveq	#0,d5
+	move.w	vwk_mode(a0),d5
+	move.l	d5,-(a7)
+
+	move.l	d0,-(a7)	; For _free_block below (and _clc_arc call)
+
+	bsr	col_pat
+	move.l	d5,-(a7)
+	move.l	d0,-(a7)
+	move.l	d4,-(a7)
+	move.l	d3,-(a7)
+	move.l	d2,-(a7)
+	move.l	d1,-(a7)
+	move.l	#9,-(a7)	; rbox
+	move.l	a0,-(a7)
+	jsr	_rounded_box	; vwk, gdb_code, x1, y1, x2, y2, colour, pattern, points, mode, interior_style
+
+	add.w	#8*4,a7
+
+	bsr	_free_block
+	add.w	#3*4,a7
+
+.rfbox_end:
+	movem.l	(a7)+,d2-d5
+	rts
 
 
 	dc.b	0,0,"v_pmarker",0
