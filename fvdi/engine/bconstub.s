5eba1e993f2998340202306ac27fdb0fc08a80cd
Standa Opichal <opichals@gmail.com>
2005-12-17 01:08:31 +0000

    Bconout(con) redirection files.

---
+++ fvdi/engine/bconstub.s
@@ -0,0 +1,44 @@
+* fVDI Bconout(con) redirection calling stub
+*
+* Copyright 2005, Standa Opichal
+* This software is licensed under the GNU General Public License.
+* Please, see LICENSE.TXT for further information.
+*****
+
+	xdef	_bconout_stub
+	xdef	_bconout_redir
+	xdef	_bconout_address
+	xref	_bconout_char
+
+	text
+
+_bconout_redir:
+	dc.w	0
+
+
+* XBRA chain for Bconout()
+	dc.b	"XBRA"
+	dc.b	"fVDI"
+_bconout_address:
+	ds.l	1
+
+_bconout_stub:
+	movem.l	d0-d2/a0-a2,-(a7)
+	move.w	6*4+4+2(a7),d0
+
+	move.l	d0,-(a7)
+	jsr	_bconout_char
+	addq.l	#4,a7
+
+	movem.l	(a7)+,d0-d2/a0-a2
+
+	tst.w	_bconout_redir
+	beq	.bconout_orig
+	
+	rts
+
+.bconout_orig:
+	move.l	_bconout_address,a0
+	jmp	(a0)
+
+	end
