c361cd277210fe4174c8a97139e23f6a3271f8a2
Johan Klockars <johan@klockars.net>
2006-01-20 09:52:03 +0000

    Beta version bump.

--- fvdi/engine/startup.c
+++ fvdi/engine/startup.c
@@ -1,7 +1,7 @@
 /*
  * fVDI startup
  *
- * $Id: startup.c,v 1.43 2005-12-20 11:34:40 johan Exp $
+ * $Id: startup.c,v 1.44 2006-01-20 09:52:03 johan Exp $
  *
  * Copyright 1999-2003, Johan Klockars 
  * This software is licensed under the GNU General Public License.
@@ -24,7 +24,7 @@
 #define SYSNAME "fvdi.sys"
 
 #define VERSION	0x0966
-#define BETA	10
+#define BETA	11
 #define VERmaj	(VERSION >> 12)
 #define VERmin	(((VERSION & 0x0f00) >> 8) * 100 + ((VERSION & 0x00f0) >> 4) * 10 + (VERSION & 0x000f))
 
