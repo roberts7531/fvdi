78f71f454d9500ffe355ee36cb5219c79d952fd8
Vincent Rivière <vincent.riviere@freesbee.fr>
2017-02-25 20:15:57 +0100

    Clip mouse sprite
    This matters when blit functions are not accelerated.
    Otherwise moving the mouse far right makes it appear left.

--- fvdi/drivers/uaegfx/uae_mous.c
+++ fvdi/drivers/uaegfx/uae_mous.c
@@ -42,20 +42,34 @@ static MFDB mouse_mfdb = {NULL, 16, 16, 1, 1, 1};
 /* We must save the mouse background */
 static short backup_data[16*16];
 static MFDB mouse_backup_mfdb = {backup_data, 16, 16, 1, 0, 16};
-static short backup_x, backup_y;
+static short backup_x, backup_y, backup_w, backup_h;
+
+static void clip_mouse(Virtual *vwk, short x, short y, short *pw, short *ph)
+{
+	short w = 16;
+	short h = 16;
+	short screen_w = vwk->real_address->screen.mfdb.width;
+	short screen_h = vwk->real_address->screen.mfdb.height;
+
+	*pw = x + w > screen_w ? screen_w - x : w;
+	*ph = y + h > screen_h ? screen_h - y : h;
+}
 
 static long draw_mouse(Virtual *vwk, short x, short y)
 {
+	short w, h;
+
 	x -= pmouse->hotspot.x;
 	y -= pmouse->hotspot.y;
+	clip_mouse(vwk, x, y, &w, &h);
 
 	/* Draw the mask, transparent mode */
 	mouse_mfdb.address = pmouse->mask;
-	c_expand_area(vwk, &mouse_mfdb, 0, 0, NULL, x, y, 16, 16, 2, pmouse->colour.background);
+	c_expand_area(vwk, &mouse_mfdb, 0, 0, NULL, x, y, w, h, 2, pmouse->colour.background);
 
 	/* Draw the data, transparent mode */
 	mouse_mfdb.address = pmouse->data;
-	c_expand_area(vwk, &mouse_mfdb, 0, 0, NULL, x, y, 16, 16, 2, pmouse->colour.foreground);
+	c_expand_area(vwk, &mouse_mfdb, 0, 0, NULL, x, y, w, h, 2, pmouse->colour.foreground);
 
 	return 1;
 }
@@ -65,7 +79,7 @@ static void hide_mouse(Virtual *vwk)
 	if (mouse_visible)
 	{
 		/* Restore the backup */
-		c_blit_area(vwk, &mouse_backup_mfdb, 0, 0, NULL, backup_x, backup_y, 16, 16, 3);
+		c_blit_area(vwk, &mouse_backup_mfdb, 0, 0, NULL, backup_x, backup_y, backup_w, backup_h, 3);
 
 		mouse_visible = 0;
 	}
@@ -79,7 +93,8 @@ static void show_mouse(Virtual *vwk, short x, short y)
 	/* Make a new backup */
 	backup_x = x - pmouse->hotspot.x;
 	backup_y = y - pmouse->hotspot.y;
-	c_blit_area(vwk, NULL, backup_x, backup_y, &mouse_backup_mfdb, 0, 0, 16, 16, 3);
+	clip_mouse(vwk, x, y, &backup_w, &backup_h);
+	c_blit_area(vwk, NULL, backup_x, backup_y, &mouse_backup_mfdb, 0, 0, backup_w, backup_h, 3);
 
 	draw_mouse(vwk, x, y);
 
