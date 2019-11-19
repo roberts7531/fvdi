6ba2180ff67391b47f5e47170531962c1cbc69df
Markus Fröschle <markus.froeschle@mahle.com>
2019-11-19 18:27:00 +0100

    remove hardcoded path

--- buildit.sh
+++ buildit.sh
@@ -12,7 +12,7 @@ FT2_DIR=${FVDI_DIR}/modules/ft2
 BPL_DIR=${FVDI_DIR}/drivers/bitplane
 ENG_DIR=${FVDI_DIR}/engine
 
-TARGET_DIR=/home/mfro/Dokumente/Development/atari/fvdi_test
+TARGET_DIR=../fvdi_test
 
 rm -f bitplane.sys
 rm -f fvdi_gnu.prg
@@ -29,11 +29,11 @@ rm -f fvdi_gnu.prg
 cp ${BPL_DIR}/bitplane.sys .
 cp ${ENG_DIR}/fvdi_gnu.prg .
 
-m68k-atari-mint-strip bitplane.sys
-m68k-atari-mint-strip fvdi_gnu.prg
+#m68k-atari-mint-strip bitplane.sys
+#m68k-atari-mint-strip fvdi_gnu.prg
 
 # copy files to test directory if existing and if CPU is supported by hatari
 if [ ${CPU} = '020' ]; then
-    cp bitplane.sys $TARGET_DIR/gemsys
-    cp fvdi_gnu.prg $TARGET_DIR/auto
+    cp bitplane.sys ${TARGET_DIR}/gemsys
+    cp fvdi_gnu.prg ${TARGET_DIR}/auto
 fi
