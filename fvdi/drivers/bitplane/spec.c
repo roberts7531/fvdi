50154d233ee77cf83f10019caa6582c64c1f54ff
Markus Fröschle <mfro@mubf.de>
2019-12-01 21:56:02 +0100

    disable FastRAM buffer

--- fvdi/drivers/bitplane/spec.c
+++ fvdi/drivers/bitplane/spec.c
@@ -3,7 +3,7 @@
  *
  * $Id: spec.c,v 1.4 2006-02-19 01:24:07 johan Exp $
  *
- * Copyright 1998-2002, Johan Klockars 
+ * Copyright 1998-2002, Johan Klockars
  * This software is licensed under the GNU General Public License.
  * Please, see LICENSE.TXT for further information.
  */
@@ -14,7 +14,7 @@
 #include "os.h"
 #include "driver.h"
 
-#if 1
+#if 0
  #define FAST		/* Write in FastRAM buffer */
  #define BOTH		/* Write in both FastRAM and on screen */
 #else
@@ -23,10 +23,10 @@
 #endif
 
 
-char red[] = {6};
-char green[] = {6};
-char blue[] = {6};
-char none[] = {0};
+char red[] = { 6 };
+char green[] = { 6 };
+char blue[] = { 6 };
+char none[] = { 0 };
 
 unsigned char tos_colours[] = {0, 1};
 
@@ -94,12 +94,12 @@ short depth = 0;
  */
 void check_token(char *token, const char **ptr)
 {
-	if (access->funcs.equal(token, "shadow"))
-		shadow = 1;
-	if (access->funcs.equal(token, "fixshape"))
-		fix_shape = 1;
-	if (access->funcs.equal(token, "norestore"))
-		no_restore = 1;
+    if (access->funcs.equal(token, "shadow"))
+        shadow = 1;
+    if (access->funcs.equal(token, "fixshape"))
+        fix_shape = 1;
+    if (access->funcs.equal(token, "norestore"))
+        no_restore = 1;
 }
 
 
@@ -110,42 +110,42 @@ void check_token(char *token, const char **ptr)
  */
 long initialize(Virtual *vwk)
 {
-	Workstation *wk;
-	char *buf;
-	int fast_w_bytes;
+    Workstation *wk;
+    char *buf;
+    int fast_w_bytes;
 #if 0
-	int i;
+    int i;
 #endif
 
-	debug = access->funcs.misc(0, 1, 0);
-	
-	vwk = me->default_vwk;	/* This is what we're interested in */
-	wk = vwk->real_address;
+    debug = access->funcs.misc(0, 1, 0);
+
+    vwk = me->default_vwk;	/* This is what we're interested in */
+    wk = vwk->real_address;
 
 #ifdef FAST
-	if (shadow) {
+    if (shadow) {
  #if 0			/* It's not clear that this is a good idea */
-		fast_w_bytes = (wk->screen.wrap + 15) & 0xfffffff0;
+        fast_w_bytes = (wk->screen.wrap + 15) & 0xfffffff0;
  #else
-		fast_w_bytes = wk->screen.wrap;
+        fast_w_bytes = wk->screen.wrap;
  #endif
-		buf = (char *)access->funcs.malloc(fast_w_bytes * wk->screen.mfdb.height + 255, 1);
-		if (buf) {
-			wk->screen.shadow.buffer = buf;
-			wk->screen.shadow.address = (void *)(((long)buf + 255) & 0xffffff00);
-			wk->screen.shadow.wrap = fast_w_bytes;
-		} else {
-			access->funcs.error("Can't allocate FastRAM!", 0);
-			wk->screen.shadow.buffer = 0;
-			wk->screen.shadow.address = 0;
-		}
+        buf = (char *)access->funcs.malloc(fast_w_bytes * wk->screen.mfdb.height + 255, 1);
+        if (buf) {
+            wk->screen.shadow.buffer = buf;
+            wk->screen.shadow.address = (void *)(((long)buf + 255) & 0xffffff00);
+            wk->screen.shadow.wrap = fast_w_bytes;
+        } else {
+            access->funcs.error("Can't allocate FastRAM!", 0);
+            wk->screen.shadow.buffer = 0;
+            wk->screen.shadow.address = 0;
+        }
  #ifndef BOTH
-		wk->screen.mfdb.address = wk->screen.shadow.address;
+        wk->screen.mfdb.address = wk->screen.shadow.address;
  #endif
-	}
+    }
 #endif
-	if (!wk->screen.shadow.address)
-		driver_name[10] = 0;
+    if (!wk->screen.shadow.address)
+        driver_name[10] = 0;
 
         if (wk->screen.pixel.width > 0)        /* Starts out as screen width */
                 wk->screen.pixel.width = (wk->screen.pixel.width * 1000L) / wk->screen.mfdb.width;
@@ -157,84 +157,84 @@ long initialize(Virtual *vwk)
                 wk->screen.pixel.height = 25400 / -wk->screen.pixel.height;
 
 #if 0
-	device.format = mode[depth].format;
-	device.clut = mode[depth].clut;
-	device.bit_depth = mode[depth].bpp;
-	device.dummy1 = (1L << mode[depth].bpp) >> 16;
-	device.colours = (1L << mode[depth].bpp) & 0xffff;
+    device.format = mode[depth].format;
+    device.clut = mode[depth].clut;
+    device.bit_depth = mode[depth].bpp;
+    device.dummy1 = (1L << mode[depth].bpp) >> 16;
+    device.colours = (1L << mode[depth].bpp) & 0xffff;
 #endif
-	device.byte_width = wk->screen.wrap;
-	device.address = wk->screen.mfdb.address;
+    device.byte_width = wk->screen.wrap;
+    device.address = wk->screen.mfdb.address;
 #if 0
-	device.bits.red = mode[depth].bits.red[0];
-	device.bits.green = mode[depth].bits.green[0];
-	device.bits.blue = mode[depth].bits.blue[0];
-	device.bits.alpha = mode[depth].bits.alpha[0];
-	device.bits.genlock = mode[depth].bits.genlock[0];
-	device.bits.unused = mode[depth].bits.unused[0];
-	device.bits.organization = mode[depth].org;
-	device.dummy2 = 0;
-	if (device.clut == 2) {
-		for(i = 0; i < mode[depth].bits.red[0]; i++)
-			device.bitnumber.red[i] = mode[depth].bits.red[i + 1];
-		for(i = mode[depth].bits.red[0]; i < 16; i++)
-			device.bitnumber.red[i] = -1;		/* Not used */
-		for(i = 0; i < mode[depth].bits.green[0]; i++)
-			device.bitnumber.green[i] = mode[depth].bits.green[i + 1];
-		for(i = mode[depth].bits.green[0]; i < 16; i++)
-			device.bitnumber.green[i] = -1;		/* Not used */
-		for(i = 0; i < mode[depth].bits.blue[0]; i++)
-			device.bitnumber.blue[i] = mode[depth].bits.blue[i + 1];
-		for(i = mode[depth].bits.blue[0]; i < 16; i++)
-			device.bitnumber.blue[i] = -1;		/* Not used */
-		for(i = 0; i < mode[depth].bits.alpha[0]; i++)
-			device.bitnumber.alpha[i] = mode[depth].bits.alpha[i + 1];
-		for(i = mode[depth].bits.alpha[0]; i < 16; i++)
-			device.bitnumber.alpha[i] = -1;		/* Not used */
-		for(i = 0; i < mode[depth].bits.genlock[0]; i++)
-			device.bitnumber.genlock[i] = mode[depth].bits.genlock[i + 1];
-		for(i = mode[depth].bits.genlock[0]; i < 16; i++)
-			device.bitnumber.genlock[i] = -1;	/* Not used */
-		for(i = 0; i < mode[depth].bits.unused[0]; i++)
-			device.bitnumber.unused[i] = mode[depth].bits.unused[i + 1];
-		for(i = mode[depth].bits.unused[0]; i < 32; i++)
-			device.bitnumber.unused[i] = -1;	/* Not used */
-		for(i = 0; i < 144; i++)
-			device.reserved[i] = 0;
-	} else {
-		for(i = 0; i < sizeof(tos_colours); i++)
-			device.vdi2pix[i] = tos_colours[i];
-		if (mode[depth].bpp == 8) {
-			for(; i < 255; i++)
-				device.vdi2pix[i] = i;
-			device.vdi2pix[255] = 15;
-		} else {
-			for(; i < 256; i++)
-				device.vdi2pix[i] = 0;
-		}
-	}
+    device.bits.red = mode[depth].bits.red[0];
+    device.bits.green = mode[depth].bits.green[0];
+    device.bits.blue = mode[depth].bits.blue[0];
+    device.bits.alpha = mode[depth].bits.alpha[0];
+    device.bits.genlock = mode[depth].bits.genlock[0];
+    device.bits.unused = mode[depth].bits.unused[0];
+    device.bits.organization = mode[depth].org;
+    device.dummy2 = 0;
+    if (device.clut == 2) {
+        for(i = 0; i < mode[depth].bits.red[0]; i++)
+            device.bitnumber.red[i] = mode[depth].bits.red[i + 1];
+        for(i = mode[depth].bits.red[0]; i < 16; i++)
+            device.bitnumber.red[i] = -1;		/* Not used */
+        for(i = 0; i < mode[depth].bits.green[0]; i++)
+            device.bitnumber.green[i] = mode[depth].bits.green[i + 1];
+        for(i = mode[depth].bits.green[0]; i < 16; i++)
+            device.bitnumber.green[i] = -1;		/* Not used */
+        for(i = 0; i < mode[depth].bits.blue[0]; i++)
+            device.bitnumber.blue[i] = mode[depth].bits.blue[i + 1];
+        for(i = mode[depth].bits.blue[0]; i < 16; i++)
+            device.bitnumber.blue[i] = -1;		/* Not used */
+        for(i = 0; i < mode[depth].bits.alpha[0]; i++)
+            device.bitnumber.alpha[i] = mode[depth].bits.alpha[i + 1];
+        for(i = mode[depth].bits.alpha[0]; i < 16; i++)
+            device.bitnumber.alpha[i] = -1;		/* Not used */
+        for(i = 0; i < mode[depth].bits.genlock[0]; i++)
+            device.bitnumber.genlock[i] = mode[depth].bits.genlock[i + 1];
+        for(i = mode[depth].bits.genlock[0]; i < 16; i++)
+            device.bitnumber.genlock[i] = -1;	/* Not used */
+        for(i = 0; i < mode[depth].bits.unused[0]; i++)
+            device.bitnumber.unused[i] = mode[depth].bits.unused[i + 1];
+        for(i = mode[depth].bits.unused[0]; i < 32; i++)
+            device.bitnumber.unused[i] = -1;	/* Not used */
+        for(i = 0; i < 144; i++)
+            device.reserved[i] = 0;
+    } else {
+        for(i = 0; i < sizeof(tos_colours); i++)
+            device.vdi2pix[i] = tos_colours[i];
+        if (mode[depth].bpp == 8) {
+            for(; i < 255; i++)
+                device.vdi2pix[i] = i;
+            device.vdi2pix[255] = 15;
+        } else {
+            for(; i < 256; i++)
+                device.vdi2pix[i] = 0;
+        }
+    }
 #endif
 
 #if 1
-	{
-	char buf[10];
+    {
+    char buf[10];
 
     access->funcs.ltoa(buf, (long) wk->screen.mfdb.width, 10);
-	access->funcs.puts(buf);
-	access->funcs.puts("x");
+    access->funcs.puts(buf);
+    access->funcs.puts("x");
     access->funcs.ltoa(buf, (long) wk->screen.mfdb.height, 10);
-	access->funcs.puts(buf);
-	access->funcs.puts("x");
+    access->funcs.puts(buf);
+    access->funcs.puts("x");
     access->funcs.ltoa(buf, (long) wk->screen.mfdb.bitplanes, 10);
-	access->funcs.puts(buf);
-	access->funcs.puts(" screen at ");
+    access->funcs.puts(buf);
+    access->funcs.puts(" screen at ");
     access->funcs.ltoa(buf, (long) wk->screen.mfdb.address, 16);
-	access->funcs.puts(buf);
-	access->funcs.puts("\x0a\x0d");
-	}
+    access->funcs.puts(buf);
+    access->funcs.puts("\x0a\x0d");
+    }
 #endif
 
-	return 1;
+    return 1;
 }
 
 /*
@@ -242,38 +242,38 @@ long initialize(Virtual *vwk)
  */
 long setup(long type, long value)
 {
-	long ret;
+    long ret;
 
-	ret = -1;
-	switch(type) {
+    ret = -1;
+    switch(type) {
 #if 0
-	case S_SCREEN:
-		if (value != -1)
-			old_screen = value;
-		ret = old_screen;
-		break;
-	case S_AESBUF:
-		if (value != -1)
-			aes_buffer = value;
-		ret = aes_buffer;
-		break;
-	case S_DOBLIT:
-		if (value != -1)
-			blit_fall_back = value;
-		ret = blit_fall_back;
-		break;
-	case S_CACHEIMG:
-		if (value != -1)
-			cache_img = value;
-		ret = cache_img;
-		break;
+    case S_SCREEN:
+        if (value != -1)
+            old_screen = value;
+        ret = old_screen;
+        break;
+    case S_AESBUF:
+        if (value != -1)
+            aes_buffer = value;
+        ret = aes_buffer;
+        break;
+    case S_DOBLIT:
+        if (value != -1)
+            blit_fall_back = value;
+        ret = blit_fall_back;
+        break;
+    case S_CACHEIMG:
+        if (value != -1)
+            cache_img = value;
+        ret = cache_img;
+        break;
 #endif
-	case Q_NAME:
-		ret = (long)driver_name;
-		break;
-	}
+    case Q_NAME:
+        ret = (long)driver_name;
+        break;
+    }
 
-	return ret;
+    return ret;
 }
 
 /*
@@ -283,9 +283,9 @@ long setup(long type, long value)
  */
 Virtual *opnwk(Virtual *vwk)
 {
-	me->default_vwk->real_address->screen.mfdb.address = (void *)Physbase();
+    me->default_vwk->real_address->screen.mfdb.address = (void *)Physbase();
 
-	return 0;
+    return 0;
 }
 
 /*
