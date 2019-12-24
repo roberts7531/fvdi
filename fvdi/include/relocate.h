9fc51964eb7755389d31fac57605b091be77f0a3
Markus Fröschle <mfro@mubf.de>
2019-12-24 14:50:28 +0100

    fix formatting

--- fvdi/include/relocate.h
+++ fvdi/include/relocate.h
@@ -29,51 +29,51 @@ typedef struct _Prgheader
 
 typedef struct _Funcs
 {
-	void CDECL (*copymem)(const void *s, void *d, long n);
-	const char* CDECL (*next_line)(const char *ptr);
-	const char* CDECL (*skip_space)(const char *ptr);
-	const char* CDECL (*get_token)(const char *ptr, char *buf, long n);
-	long  CDECL (*equal)(const char *str1, const char *str2);
-	long  CDECL (*length)(const char *text);
-	void CDECL (*copy)(const char *src, char *dest);
-	void CDECL (*cat)(const char *src, char *dest);
-	long  CDECL (*numeric)(long ch);
-	long CDECL (*atol)(const char *text);
-	void CDECL (*error)(const char *text1, const char *text2);
-    void* CDECL (*malloc)(long size, long type);                    /* Uses Mxalloc if possible */
-	long CDECL (*free)(void *addr);
-	long CDECL (*puts)(const char *text);
-	void CDECL (*ltoa)(char *buf, long n, unsigned long base);
-    long CDECL (*get_cookie)(const char *cname, long super);
-    long CDECL (*set_cookie)(const char *cname, long value);
-	long CDECL (*fixup_font)(Fontheader *font, char *buffer, long flip);
-	long CDECL (*unpack_font)(Fontheader *header, long format);
-	long CDECL (*insert_font)(Fontheader **first_font, Fontheader *new_font);
-	long CDECL (*get_size)(const char *name);
-    char* CDECL (*allocate_block)(size_t size);
-	void CDECL (*free_block)(void *address);
-	void CDECL (*cache_flush)(void);
-	long CDECL (*misc)(long func, long par, const char *token);
-	long CDECL (*event)(long id_type, long data);
+    void (*copymem)(const void *s, void *d, long n);
+    const char * (*next_line)(const char *ptr);
+    const char * (*skip_space)(const char *ptr);
+    const char * (*get_token)(const char *ptr, char *buf, long n);
+    long (*equal)(const char *str1, const char *str2);
+    long (*length)(const char *text);
+    void (*copy)(const char *src, char *dest);
+    void (*cat)(const char *src, char *dest);
+    long (*numeric)(long ch);
+    long (*atol)(const char *text);
+    void (*error)(const char *text1, const char *text2);
+    void * (*malloc)(long size, long type);                    /* Uses Mxalloc if possible */
+    long (*free)(void *addr);
+    long (*puts)(const char *text);
+    void (*ltoa)(char *buf, long n, unsigned long base);
+    long (*get_cookie)(const char *cname, long super);
+    long (*set_cookie)(const char *cname, long value);
+    long (*fixup_font)(Fontheader *font, char *buffer, long flip);
+    long (*unpack_font)(Fontheader *header, long format);
+    long (*insert_font)(Fontheader **first_font, Fontheader *new_font);
+    long (*get_size)(const char *name);
+    void * (*allocate_block)(size_t size);
+    void (*free_block)(void *address);
+    void (*cache_flush)(void);
+    long (*misc)(long func, long par, const char *token);
+    long (*event)(long id_type, long data);
 } Funcs;
 
 typedef struct _Vars
 {
-	long	*version;
-	char	*name;
+    long *version;
+    char *name;
 } Vars;
 
 typedef struct _Access
 {
-	Funcs	funcs;
-	Vars	vars;	
+    Funcs funcs;
+    Vars vars;
 } Access;
 
 typedef struct _Locator
 {
-	char 	magic[10];
-	short	version;
-	long  CDECL (*init)(Access *, Driver *, Virtual *, char *);
+    char magic[10];
+    short version;
+    long  (*init)(Access *, Driver *, Virtual *, char *);
 } Locator;
 
-#endif
+#endif /* RELOCATE_H */
