3b5d7a0f383870141c3709163960ccb414c5934a
Thorsten Otto <admin@tho-otto.de>
2020-05-11 13:17:37 +0200

    Avoid type-punned pointers

--- fvdi/engine/setup.c
+++ fvdi/engine/setup.c
@@ -47,32 +47,27 @@ Virtual *initialize_vdi(void)
     Virtual *vwk, *dummy_vwk;
     char *tmp;
     int i;
-
-    typedef struct fake_wkst
-    {
-        struct Workstation *wkst;
-        short stdhandle;
-        Function functab[257];
-    } FakeWkst;
+    long func_tab_start;
 
     /*
-    * non_fvdi_wk  - A dummy workstation with all function pointers set to 'bad_or_non_vdi_handle'.
-    *                The function table is the only part that actually exists in memory.
-    * non_fvdi_vwk - A dummy virtual workstation with the above as 'base'.
-    *                Only the pointer to the above and the 'standard handle' (-1) actually exists.
-    *                Used for all unallocated entries in the handle table.
-    */
+     * non_fvdi_wk  - A dummy workstation with all function pointers set to 'bad_or_non_vdi_handle'.
+     *                The function table is the only part that actually exists in memory.
+     * non_fvdi_vwk - A dummy virtual workstation with the above as 'base'.
+     *                Only the pointer to the above and the 'standard handle' (-1) actually exists.
+     *                Used for all unallocated entries in the handle table.
+     */
 
-    if ((tmp = malloc(sizeof(FakeWkst))) == NULL)
+    if ((tmp = (char *) malloc(sizeof(Workstation *) + sizeof(short) + 257 * sizeof(Function))) == NULL)
         return 0;
 
     /* func_tab_start is the offset of the function table in the block just allocated */
-    dummy_wk = (Workstation *) (tmp + offsetof(FakeWkst, functab) - offsetof(Workstation, dummy));
+    func_tab_start = (long) &((Workstation *) tmp)->function - (long) tmp - sizeof(Function);
+    dummy_wk = (Workstation *) (tmp + sizeof(Workstation *) + sizeof(short) - func_tab_start);
     dummy_vwk = (Virtual *) tmp;
-    dummy_vwk->real_address = (void *) dummy_wk;
+    dummy_vwk->real_address = dummy_wk;
     dummy_vwk->standard_handle = -1;
 
-    for (i = -1; i < 256; i ++)
+    for (i = -1; i < 256; i++)
     {
         dummy_wk->function[i].retvals[0] = 0;
         dummy_wk->function[i].retvals[1] = 0;
@@ -85,27 +80,25 @@ Virtual *initialize_vdi(void)
     for (i = 0; i < HANDLES; i++)
         handle[i] = dummy_vwk;
 
-    if (!(wk = malloc(sizeof(Workstation))))
+    if ((wk = (Workstation *) malloc(sizeof(Workstation))) == NULL)
     {
         free(tmp);
-
         return 0;
     }
 
-    if (!(vwk = malloc(sizeof(Virtual))))
+    if ((vwk = (Virtual *) malloc(sizeof(Virtual))) == NULL)
     {
         free(wk);
         free(tmp);
-
         return 0;
     }
 
     /*
-    * Set up the default real and virtual workstations.
-    * These are never actually used, but can be considered templates with some
-    * reasonable initial values.
-    * The virtual workstation is both returned and stored as 'default_virtual'.
-    */
+     * Set up the default real and virtual workstations.
+     * These are never actually used, but can be considered templates with some
+     * reasonable initial values.
+     * The virtual workstation is both returned and stored as 'default_virtual'.
+     */
 
     /* Screen */
     wk->driver = 0;
@@ -119,32 +112,32 @@ Virtual *initialize_vdi(void)
     wk->screen.shadow.buffer = 0;
     wk->screen.shadow.address = 0;
     wk->screen.shadow.wrap = 0;
-    /* */
-    wk->screen.pixel.width = 238;               /* Used to be 353 */
+
+    wk->screen.pixel.width = 238;       /* Used to be 353 */
     wk->screen.pixel.height = 149;
-    /* */
+
     wk->screen.coordinates.course = 0;
-    /* */
+
     wk->screen.coordinates.min_x = 0;
     wk->screen.coordinates.min_y = 0;
-    /* */
+
     wk->writing.fonts = 0;
     wk->writing.first_font = 0;
-    wk->writing.effects = 0x1f;                 /* Outline/underline/italic/light/bold supported */
+    wk->writing.effects = 0x1f;         /* Outline/underline/italic/light/bold supported */
     wk->writing.rotation.possible = 0;
     wk->writing.rotation.type = 0;
     wk->writing.justification = 0;
-    /* */
-    wk->writing.size.possibilities = 3;      /* Get these some real way! */
+
+    wk->writing.size.possibilities = 3; /* Get these some real way! */
     wk->writing.size.height.min = 4;
     wk->writing.size.height.max = 13;
     wk->writing.size.width.min = 5;
     wk->writing.size.width.max = 7;
-    /* */
-    wk->drawing.primitives.supported = 10;    /* Everything */
-    wk->drawing.primitives.attributes = ((3 + 1) <<  0) + ((0 + 1) <<  3) + ((3 + 1) <<  6) +
-            ((3 + 1) <<  9) + ((3 + 1) << 12) + ((0 + 1) << 15) +
-            ((3L + 1) << 18) + ((0L + 1) << 21) + ((0L + 1) << 24) + ((2L + 1) << 27);
+
+    wk->drawing.primitives.supported = 10;  /* Everything */
+    wk->drawing.primitives.attributes = ((3 + 1) << 0) + ((0 + 1) << 3) + ((3 + 1) << 6) +
+        ((3 + 1) << 9) + ((3 + 1) << 12) + ((0 + 1) << 15) +
+        ((3L + 1) << 18) + ((0L + 1) << 21) + ((0L + 1) << 24) + ((2L + 1) << 27);
     wk->drawing.rubber_banding = 0;
     wk->drawing.flood_fill = 0;
     wk->drawing.writing_modes = 4;
@@ -159,7 +152,7 @@ Virtual *initialize_vdi(void)
     wk->drawing.marker.size.width.max = 120;
     wk->drawing.line.types = 7;
     wk->drawing.line.wide.width.possibilities = 0;
-    wk->drawing.line.wide.width.max = 31;        /* Was 255 */
+    wk->drawing.line.wide.width.max = 31;   /* Was 255 */
     wk->drawing.line.wide.width.min = 1;
     wk->drawing.line.wide.types_possible = 1;
     wk->drawing.line.wide.writing_modes = 0;
@@ -173,7 +166,7 @@ Virtual *initialize_vdi(void)
     /* 16x16 op/s */
     wk->various.input_type = 0;
     /* Nedsvartning */
-    wk->various.max_ptsin = 1024;                /* Was -1 */
+    wk->various.max_ptsin = 1024;       /* Was -1 */
     wk->various.max_intin = -1;
     wk->various.buttons = 0;
     wk->various.cursor_movement = 0;
@@ -183,37 +176,35 @@ Virtual *initialize_vdi(void)
     wk->various.workstation_type = 0;
     /* Console */
     /* Mouse */
-    wk->mouse.type = 0;                         /* Default to old VDI mouse */
+    wk->mouse.type = 0;                 /* Default to old VDI mouse */
     wk->mouse.hide = 0;
     wk->mouse.buttons = 0;
     wk->mouse.forced = 0;
     wk->mouse.position.x = 64;
     wk->mouse.position.y = 64;
     wk->vector.motion = mouse_move;
-    wk->vector.draw   = do_nothing;
+    wk->vector.draw = do_nothing;
     wk->vector.button = do_nothing;
-    wk->vector.wheel  = do_nothing;
+    wk->vector.wheel = do_nothing;
     wk->vector.vblank = mouse_timer;
     wk->vblank.real = 0;
     wk->vblank.frequency = 50;
     wk->r.set_palette = 0;
-    wk->r.get_colour  = 0;
-    wk->r.set_pixel   = 0;
-    wk->r.get_pixel   = 0;
-    wk->r.line     = &default_line;
-    wk->r.expand   = &default_expand;
-    wk->r.fill     = &default_fill;
+    wk->r.get_colour = 0;
+    wk->r.set_pixel = 0;
+    wk->r.get_pixel = 0;
+    wk->r.line = default_line;
+    wk->r.expand = default_expand;
+    wk->r.fill = default_fill;
     wk->r.fillpoly = 0;
-    wk->r.blit     = &default_blit;
-    wk->r.text     = &default_text;
-    wk->r.mouse    = 0;
+    wk->r.blit = default_blit;
+    wk->r.text = default_text;
+    wk->r.mouse = 0;
 
-    copymem(&default_functions[-1], &wk->function[-1], 257 * sizeof(Function));
-
-    wk->opcode5_count = * (short *) ((long) default_opcode5 - 2);
+    copymem(default_functions - 1, (char *)((long)wk->function - sizeof(Function)), 257 * sizeof(Function));
+    wk->opcode5_count = *(short *) ((long) default_opcode5 - 2);
     copymem(default_opcode5, wk->opcode5, (wk->opcode5_count + 1) * sizeof(void *));
-
-    wk->opcode11_count = * (short *) ((long) default_opcode11 - 2);
+    wk->opcode11_count = *(short *) ((long) default_opcode11 - 2);
     copymem(default_opcode11, wk->opcode11, (wk->opcode11_count + 1) * sizeof(void *));
 
     vwk->clip.on = 0;
@@ -312,7 +303,7 @@ static void copy_setup(Virtual *def, int vwk_no, short intout[], short ptsout[])
     Virtual *vwk;
     short tmp;
 
-    vwk = malloc(sizeof(Virtual) + 32);  /* Extra for fill pattern */
+    vwk = (Virtual *) malloc(sizeof(Virtual) + 32); /* Extra for fill pattern */
     if (!vwk)
         return;
 
@@ -327,66 +318,66 @@ static void copy_setup(Virtual *def, int vwk_no, short intout[], short ptsout[])
         setup_colours(vwk);
 
     set_inout(0, 0, intout, ptsout);
-    vdi(vwk_no, 35, 0, 0);     /* vql_attributes */
+    vdi(vwk_no, 35, 0, 0);              /* vql_attributes */
     set_inout(0, 0, 0, 0);
-    int_in[0] = intout[2];        /* Mode */
-    fvdi(vwk_no, 32, 0, 1);    /* vswr_mode */
-    int_in[0] = intout[0];        /* Type */
-    fvdi(vwk_no, 15, 0, 1);    /* vsl_type */
-    int_in[0] = intout[1];        /* Colour */
-    fvdi(vwk_no, 16, 0, 1);    /* vsl_color */
-    int_in[0] = intout[3];        /* Beginning */
-    int_in[1] = intout[4];        /* End */
-    fvdi(vwk_no, 108, 0, 2);   /* vsl_ends */
-    pts_in[0] = ptsout[0];        /* Width */
+    int_in[0] = intout[2];              /* Mode */
+    fvdi(vwk_no, 32, 0, 1);             /* vswr_mode */
+    int_in[0] = intout[0];              /* Type */
+    fvdi(vwk_no, 15, 0, 1);             /* vsl_type */
+    int_in[0] = intout[1];              /* Colour */
+    fvdi(vwk_no, 16, 0, 1);             /* vsl_color */
+    int_in[0] = intout[3];              /* Beginning */
+    int_in[1] = intout[4];              /* End */
+    fvdi(vwk_no, 108, 0, 2);            /* vsl_ends */
+    pts_in[0] = ptsout[0];              /* Width */
     pts_in[1] = 0;
-    fvdi(vwk_no, 16, 1, 0);         /* vsl_width */
+    fvdi(vwk_no, 16, 1, 0);             /* vsl_width */
 
     set_inout(0, 0, intout, ptsout);
-    vdi(vwk_no, 36, 0, 0);     /* vqm_attributes */
+    vdi(vwk_no, 36, 0, 0);              /* vqm_attributes */
     set_inout(0, 0, 0, 0);
-    int_in[0] = intout[0];        /* Type */
-    fvdi(vwk_no, 18, 0, 1);    /* vsm_type */
-    int_in[0] = intout[1];        /* Colour */
-    fvdi(vwk_no, 20, 0, 1);    /* vsm_color */
-    pts_in[0] = ptsout[0];        /* Width (0 according to docs) */
-    pts_in[1] = ptsout[1];        /* Height */
-    fvdi(vwk_no, 19, 1, 0);    /* vsm_height */
+    int_in[0] = intout[0];              /* Type */
+    fvdi(vwk_no, 18, 0, 1);             /* vsm_type */
+    int_in[0] = intout[1];              /* Colour */
+    fvdi(vwk_no, 20, 0, 1);             /* vsm_color */
+    pts_in[0] = ptsout[0];              /* Width (0 according to docs) */
+    pts_in[1] = ptsout[1];              /* Height */
+    fvdi(vwk_no, 19, 1, 0);             /* vsm_height */
 
     set_inout(0, 0, intout, 0);
-    vdi(vwk_no, 37, 0, 0);     /* vqf_attributes */
+    vdi(vwk_no, 37, 0, 0);              /* vqf_attributes */
     set_inout(0, 0, 0, 0);
-    int_in[0] = intout[0];        /* Interior */
-    fvdi(vwk_no, 23, 0, 1);    /* vsf_interior */
-    int_in[0] = intout[1];        /* Colour */
-    fvdi(vwk_no, 25, 0, 1);    /* vsf_color */
-    int_in[0] = intout[2];        /* Style */
-    fvdi(vwk_no, 24, 0, 1);    /* vsf_style */
-    int_in[0] = intout[4];        /* Perimeter */
-    fvdi(vwk_no, 104, 0, 1);   /* vsf_perimeter */
+    int_in[0] = intout[0];              /* Interior */
+    fvdi(vwk_no, 23, 0, 1);             /* vsf_interior */
+    int_in[0] = intout[1];              /* Colour */
+    fvdi(vwk_no, 25, 0, 1);             /* vsf_color */
+    int_in[0] = intout[2];              /* Style */
+    fvdi(vwk_no, 24, 0, 1);             /* vsf_style */
+    int_in[0] = intout[4];              /* Perimeter */
+    fvdi(vwk_no, 104, 0, 1);            /* vsf_perimeter */
 
     set_inout(0, 0, intout, 0);
-    vdi(vwk_no, 38, 0, 0);     /* vqt_attributes */
+    vdi(vwk_no, 38, 0, 0);              /* vqt_attributes */
     set_inout(0, 0, 0, 0);
-    int_in[0] = intout[0];        /* Font */
-    fvdi(vwk_no, 21, 0, 1);    /* vst_font */
-    int_in[0] = intout[1];        /* Colour */
-    fvdi(vwk_no, 22, 0, 1);    /* vst_color */
-    int_in[0] = intout[2];        /* Rotation */
-    fvdi(vwk_no, 13, 0, 1);    /* vst_rotation */
-    int_in[0] = intout[3];        /* Horizontal alignment */
-    int_in[1] = intout[4];        /* Vertical alignment */
-    fvdi(vwk_no, 39, 0, 2);    /* vst_alignment */
+    int_in[0] = intout[0];              /* Font */
+    fvdi(vwk_no, 21, 0, 1);             /* vst_font */
+    int_in[0] = intout[1];              /* Colour */
+    fvdi(vwk_no, 22, 0, 1);             /* vst_color */
+    int_in[0] = intout[2];              /* Rotation */
+    fvdi(vwk_no, 13, 0, 1);             /* vst_rotation */
+    int_in[0] = intout[3];              /* Horizontal alignment */
+    int_in[1] = intout[4];              /* Vertical alignment */
+    fvdi(vwk_no, 39, 0, 2);             /* vst_alignment */
 
     set_inout(0, 0, intout, ptsout);
-    vdi(vwk_no, 131, 0, 0);    /* vqt_fontinfo */
+    vdi(vwk_no, 131, 0, 0);             /* vqt_fontinfo */
     set_inout(0, 0, 0, 0);
     pts_in[0] = 0;
-    pts_in[1] = ptsout[9];        /* Baseline to top line */
-    fvdi(vwk_no, 12, 1, 0);    /* vst_height */
+    pts_in[1] = ptsout[9];              /* Baseline to top line */
+    fvdi(vwk_no, 12, 1, 0);             /* vst_height */
 
     set_inout(0, 0, intout, ptsout);
-    vdi(vwk_no, 102, 0, 1);    /* vq_extnd */
+    vdi(vwk_no, 102, 0, 1);             /* vq_extnd */
     set_inout(0, 0, 0, 0);
     tmp = int_out[19];
     if (tmp & 0xfffe)
@@ -408,10 +399,10 @@ static void copy_setup(Virtual *def, int vwk_no, short intout[], short ptsout[])
     else
         tmp = 0;
     int_in[0] = tmp;
-    fvdi(vwk_no, 129, 2, 1);    /* vs_clip */
+    fvdi(vwk_no, 129, 2, 1);            /* vs_clip */
 
     /* This should perhaps only be done per Workstation */
-    vdi(vwk_no, 124, 0, 0);    /* vq_mouse */
+    vdi(vwk_no, 124, 0, 0);             /* vq_mouse */
     vwk->real_address->mouse.position.x = pts_out[0];
     vwk->real_address->mouse.position.y = pts_out[1];
     vwk->real_address->mouse.hotspot.x = 0;
@@ -421,31 +412,47 @@ static void copy_setup(Virtual *def, int vwk_no, short intout[], short ptsout[])
 
 void link_mouse_routines(void)
 {
-    * (long *) &control[7] = (long) mouse_move;
+    /* vex_curv */
+    long *lp;
+
+    lp = (long *) &control[7];
+    *lp = (long) mouse_move;
     if (booted && !fakeboot)
         sub_vdi(old_wk_handle, 127, 0, 0);
     else
         vdi(old_wk_handle, 127, 0, 0);
-    old_curv = (void *) * (long *) &control[9];
-    * (long *) &control[7] = (long) mouse_timer;
+    lp = (long *)&control[9];
+    old_curv = (void *)(*lp);
+    lp = (long *)&control[9];
+    old_curv = (void *)(*lp);
+    /* vex_timv */
+    lp = (long *) &control[7];
+    *lp = (long) mouse_timer;
     if (booted && !fakeboot)
         sub_vdi(old_wk_handle, 118, 0, 0);
     else
         vdi(old_wk_handle, 118, 0, 0);
-    old_timv = (void *) * (long *) &control[9];
+    lp = (long *)&control[9];
+    old_timv = (void *)(*lp);
 }
 
 
 void unlink_mouse_routines(void)
 {
+    long *lp;
+
+    /* vex_curv */
     if (old_curv)
     {
-        * (long *) &control[7] = (long) old_curv;
+        lp = (long *) &control[7];
+        *lp = (long) old_curv;
         vdi(old_wk_handle, 127, 0, 0);
     }
+    /* vex_timv */
     if (old_timv)
     {
-        * (long *) &control[7] = (long) old_timv;
+        lp = (long *) &control[7];
+        *lp = (long) old_timv;
         vdi(old_wk_handle, 118, 0, 0);
     }
 }
@@ -456,14 +463,14 @@ void setup_vbl_handler(void)
     int n;
     long addr;
 
-    n = get_l(0x452L) & 0xffffL;   /* nvbls */
-    addr = get_l(0x456);           /* vblqueue */
+    n = get_l(0x452L) & 0xffffL;        /* nvbls */
+    addr = get_l(0x456);                /* vblqueue */
     for (; n > 0; n--)
     {
         if (get_l(addr) == 0)
         {
             old_timv = do_nothing;
-            set_l(addr, (long)vbl_handler);
+            set_l(addr, (long) vbl_handler);
             vbl_handler_installed = 1;
             break;
         }
@@ -477,8 +484,8 @@ void shutdown_vbl_handler(void)
     int n;
     long addr;
 
-    n = get_l(0x452L) & 0xffffL;   /* nvbls */
-    addr = get_l(0x456);           /* vblqueue */
+    n = get_l(0x452L) & 0xffffL;        /* nvbls */
+    addr = get_l(0x456);                /* vblqueue */
     for (; n > 0; n--)
     {
         if (get_l(addr) == (long) vbl_handler)
@@ -512,26 +519,25 @@ void copy_workstations(Virtual *def, long really_copy)
     appl_exit();
 
     /*
-    * Try opening a large amount of new virtual workstations.
-    * The handles we don't get represent the already opened ones.
-    */
-
+     * Try opening a large amount of new virtual workstations.
+     * The handles we don't get represent the already opened ones.
+     */
     n = 0;
     do
     {
         new_handle = call_v_opnvwk(old_wk_handle, intout, ptsout);
         handles[n++] = new_handle;
-    } while (new_handle <= MAX_OLD_HANDLE);
+    } while (n < MAX_OLD_HANDLE && new_handle <= MAX_OLD_HANDLE);
     for (i = n - 1; i >= 0; i--)
         call_v_clsvwk(handles[i]);
 
     /*
-    * Copy all the virtual screen workstations that were found,
-    * and make dummy virtuals for everything else.
-    */
+     * Copy all the virtual screen workstations that were found,
+     * and make dummy virtuals for everything else.
+     */
 
-    if (n <= MAX_OLD_HANDLE)                      /* This is for the dummy virtuals */
-        tmp = malloc((sizeof(Workstation *) + sizeof(short)) * (MAX_OLD_HANDLE - n + 1));
+    if (n <= MAX_OLD_HANDLE)            /* This is for the dummy virtuals */
+        tmp = (char *) malloc((sizeof(Workstation *) + sizeof(short)) * (MAX_OLD_HANDLE - n + 1));
 
     last_handle = 0;
     for (j = 0; j < n; j++)
@@ -541,19 +547,20 @@ void copy_workstations(Virtual *def, long really_copy)
         {
             vq_extnd(i, 1, intout, ptsout);
             if (!intout[0])
-            {                                         /* Not a screen device? */
+            {                           /* Not a screen device? */
                 handle[i] = (Virtual *) tmp;
                 tmp += sizeof(Workstation *) + sizeof(short);
                 handle[i]->real_address = (void *) non_fvdi_wk;
-                handle[i]->standard_handle = i | 0x8000;               /* Don't try to open virtuals for this one */
+                handle[i]->standard_handle = i | 0x8000;    /* Don't try to open virtuals for this one */
                 continue;
             }
             if (really_copy)
-                copy_setup(def, i, intout, ptsout);
-            else
             {
-                handle[i] = (Virtual *) tmp;                            /* If we're not copying, */
-                tmp += sizeof(Workstation *) + sizeof(short);          /*  set up dummies for pass-through. */
+                copy_setup(def, i, intout, ptsout);
+            } else
+            {
+                handle[i] = (Virtual *) tmp;    /* If we're not copying, */
+                tmp += sizeof(Workstation *) + sizeof(short);   /*  set up dummies for pass-through. */
                 handle[i]->real_address = (void *) non_fvdi_wk;
                 handle[i]->standard_handle = i;
             }
