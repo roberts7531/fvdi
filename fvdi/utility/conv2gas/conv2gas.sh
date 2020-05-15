dbe06f56d7cd495c487a1de87d0402390f895378
Thorsten Otto <admin@tho-otto.de>
2020-05-15 18:03:19 +0200

    Generate preprocessed files atomically, to avoid problems with parallel makes

--- fvdi/utility/conv2gas/conv2gas.sh
+++ fvdi/utility/conv2gas/conv2gas.sh
@@ -5,74 +5,77 @@ case "$file" in
   *.s) out=`dirname $file`/`basename $file .s`.gnu.s ;;
   *) out="$file.gnu" ;;
 esac
-tmp=`basename $file`.tmp
+tmp1=`basename $file`.tm1
+tmp2=`basename $file`.tm2
+
+trap "rm -f $tmp1 $tmp2" 0 1 2 3 15
 
 # Comments with * in the first position on the line
-sed "s%^\*.*%%" $file >$out
-mv $out $tmp
+sed "s%^\*.*%%" $file >$tmp2
+mv $tmp2 $tmp1
 
 # Comments after ; separator
-sed "s%;.*$%%" $tmp >$out
-mv $out $tmp
+sed "s%;.*$%%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
 
 # Conditional compilation
-sed "s%^\\([ 	][ 	]*\\)ifnd%\1.ifndef%" $tmp >$out
-mv $out $tmp
-sed "s%^\\([ 	][ 	]*\\)ifd%\1.ifdef%" $tmp >$out
-mv $out $tmp
-sed "s%^\\([ 	][ 	]*\\)ifne%\1.ifne%" $tmp >$out
-mv $out $tmp
-sed "s%^\\([ 	][ 	]*\\)ifeq%\1.ifeq%" $tmp >$out
-mv $out $tmp
-sed "s%^\\([ 	][ 	]*\\)ifge%\1.ifge%" $tmp >$out
-mv $out $tmp
-sed "s%^\\([ 	][ 	]*\\)else%\1.else%" $tmp >$out
-mv $out $tmp
-sed "s%^\\([ 	][ 	]*\\)endc%\1.endif%" $tmp >$out
-mv $out $tmp
+sed "s%^\\([ 	][ 	]*\\)ifnd%\1.ifndef%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^\\([ 	][ 	]*\\)ifd%\1.ifdef%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^\\([ 	][ 	]*\\)ifne%\1.ifne%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^\\([ 	][ 	]*\\)ifeq%\1.ifeq%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^\\([ 	][ 	]*\\)ifge%\1.ifge%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^\\([ 	][ 	]*\\)else%\1.else%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^\\([ 	][ 	]*\\)endc%\1.endif%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
 
 # Constants
-sed "s%^\\([^ 	]*\\)[ 	][ 	]*equ[ 	][ 	]*%	.equ \\1,%" $tmp >$out
-mv $out $tmp
-sed "s%^\\([^ 	]*\\)[ 	][ 	]*set[ 	][ 	]*%	.set	\\1,%" $tmp >$out
-mv $out $tmp
+sed "s%^\\([^ 	]*\\)[ 	][ 	]*equ[ 	][ 	]*%	.equ \\1,%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^\\([^ 	]*\\)[ 	][ 	]*set[ 	][ 	]*%	.set	\\1,%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
 
 # Sections
-sed "s%^\\([ 	][ 	]*\\)text%\1.text%" $tmp >$out
-mv $out $tmp
-sed "s%^\\([ 	][ 	]*\\)data%\1.data%" $tmp >$out
-mv $out $tmp
-sed "s%^\\([ 	][ 	]*\\)bss%\1.bss%" $tmp >$out
-mv $out $tmp
+sed "s%^\\([ 	][ 	]*\\)text%\1.text%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^\\([ 	][ 	]*\\)data%\1.data%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^\\([ 	][ 	]*\\)bss%\1.bss%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
 
 # Includes
-sed "s%\.inc\"%.inc.gnu\"%" $tmp >$out
-mv $out $tmp
-sed "s%\\([ 	][ 	]*\\)include%\1.include%" $tmp >$out
-mv $out $tmp
+sed "s%\.inc\"%.inc.gnu\"%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%\\([ 	][ 	]*\\)include%\1.include%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
 
 # Hexadecimal numbers
-sed "s%\\$%0x%" $tmp >$out
-mv $out $tmp
+sed "s%\\$%0x%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
 
-sed "s%^.loop1:$%1:%" $tmp >$out
-mv $out $tmp
-sed "s%^.loop2:$%2:%" $tmp >$out
-mv $out $tmp
-sed "s%^.loop2_end:$%3:%" $tmp >$out
-mv $out $tmp
-sed "s%,.loop1$%,1b%" $tmp >$out
-mv $out $tmp
-sed "s%,.loop2$%,2b%" $tmp >$out
-mv $out $tmp
-sed "s%.loop2_end$%3f%" $tmp >$out
-mv $out $tmp
+sed "s%^.loop1:$%1:%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^.loop2:$%2:%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%^.loop2_end:$%3:%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%,.loop1$%,1b%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%,.loop2$%,2b%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
+sed "s%.loop2_end$%3f%" $tmp1 >$tmp2
+mv $tmp2 $tmp1
 
 # Things that would be hard do deal with using sed
-`dirname $0`/conv2gas $tmp >$out
+`dirname $0`/conv2gas $tmp1 >$tmp2
 
 # Remove temporary file
-rm $tmp
+mv $tmp2 $out
+rm $tmp1
 
 done
-
